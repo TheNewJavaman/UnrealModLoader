4a85f499f3e45096d3ffc0a45637d1e2fa59dfb3
Gabriel Pizarro <gpizarro@javaman.net>
2022-02-15 10:13:48 -0800

    Fix VS 2019 file deletions

--- GhostrunnerVRMod/DX11Callbacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ExampleMod.h"
-
-void OnDraw(D3D11DrawOrig** orig, ID3D11DeviceContext* pContext, UINT VertexCount, UINT StartVertexLocation);
-
-void OnDrawIndexed(ID3D11DeviceContext* pContext, UINT IndexCount, UINT StartIndexLocation, INT BaseVertexLocation);
-
-void OnDrawIndexedInstanced(ID3D11DeviceContext* pContext, UINT IndexCountPerInstance, UINT InstanceCount, UINT StartIndexLocation, INT BaseVertexLocation, UINT StartInstanceLocation);
