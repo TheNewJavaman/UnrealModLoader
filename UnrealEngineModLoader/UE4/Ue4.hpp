ca984659b0173b7549ea8a7d20e38f1615d3a537
Gabriel Pizarro <gpizarro@javaman.net>
2022-02-16 07:53:52 -0800

    Revert "Add event trigger helper function"
    This reverts commit 52a6087321dbc7f2930e493442bdcc1be858ad35.

--- UnrealEngineModLoader/UE4/Ue4.hpp
+++ UnrealEngineModLoader/UE4/Ue4.hpp
@@ -76,20 +76,4 @@ namespace UE4
 		#endif
 
 	}
-
-	static void TriggerEvent(std::wstring FunctionName, std::vector<std::wstring> FunctionArgs)
-	{
-		std::wstring command = FunctionName;
-		for (int i = 0; i < FunctionArgs.size(); i++) 
-		{
-			command += L" " + FunctionArgs.at(i);
-		}
-		for (int i = 0; i < Global::GetGlobals()->ModInfoList.size(); i++)
-		{
-			if (Global::GetGlobals()->ModInfoList[i].CurrentModActor)
-			{
-				Global::GetGlobals()->ModInfoList[i].CurrentModActor->CallFunctionByNameWithArguments(command.c_str(), nullptr, NULL, true);
-			}
-		}
-	}
 }
